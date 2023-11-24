eeafddfe50a1daca1c0eded0835488b4e4c5bf7f
DorinVieru <dorino.vieru@gmail.com>
2023-11-24 09:55:35 +0100

    [UPDATE] Improve code js mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,19 +1,29 @@
 // INSERIRE IL PROMPT PER PERMETTERE L'INSERIMENTO DELLA MAIL
 let mail = prompt("Inserisci la tua mail");
 
+// CREO ARRAY CON LISTA MAIL
 let mails = ["sono@latuamail.com", "info@latuamail.com", "dorin@latuamail.com", "sono@lamiamail.com", "si@no.com"];
 
 let message;
 
+// DICHIARO LA VARIABILE FLAG PER VERIFICARE SE LA VARIABILE E' CORRETTA
+let flag = false;
+
+// CREO LA CONDIZIONE PER VERIFICARE LA CORRETTA MAIL
 for (let i=0; i<mails.length; i++){
     if(mail == mails[i]){
-        message = "Sei autorizzato. Bentornato/a";
-        // document.getElementById("autorizzato").innerHTML = message;
-    }
-    else{
-        message = "Non sei autorizzato/a ad accedere a questa area riservata. Fuori!"
-        // document.getElementById("non").innerHTML = message;
+        flag = true;
     }
 }
 
-console.log(message);
+// VERIFICO LA CONDIZIONE
+if (flag == true){
+    message = "Sei autorizzato. Bentornato/a";
+    document.getElementById("autorizzato").innerHTML = message;
+    console.log(message);
+}
+else{
+    message = "Non sei autorizzato/a ad accedere a questa area riservata. Fuori!"
+    document.getElementById("non").innerHTML = message;
+    console.log(message);
+}
